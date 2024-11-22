f7b3d0c6e4e58e533a00a04dade378a918be92a9
jwelles0311 <jwelles0311@gmail.com>
2024-11-22 13:44:24 -0300

    Cadastro de Clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('clientes/list', views.ClientesListView.as_view(), name='clientes_list'),
+    path('clientes/create', views.ClientesCreateView.as_view(),
+         name='clientes_create'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,11 +1,27 @@
-from django.views.generic import ListView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView, CreateView
 from . import models
 
 # Create your views here.
+
 
 class ClientesListView(ListView):
     model = models.Companies
     template_name = 'clientes_list.html'
     context_object_name = 'clientes'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        corporate_reason = self.request.GET.get('corporate_reason')
 
+        if corporate_reason:
+            queryset = queryset.filter(
+                corporate_reason__contains=corporate_reason)
+        return queryset
+
+
+class ClientesCreateView(CreateView):
+    model = models.Companies
+    template_name = 'clientes_list.html'
+    context_object_name = 'clientes'
